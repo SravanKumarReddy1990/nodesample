8889184f01095bc81e95c487416546c68ed7c7d0
sravan <bskreddy1990@gmail.com>
2019-09-27 10:59:31 +0530

    Initial release

--- nodes/app.js
+++ nodes/app.js
@@ -5,6 +5,6 @@ var localTutor=require('./NodeTutorial.js');
 localTutor.NodeTutorial();
 localTutor.NodeTutorial.pTutor();
 
-Or use this code
+
 var tut = new localTutor.NodeTutorial();  // Create and save object
 tut.pTutor();
